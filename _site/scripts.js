e65e5cbecb18a46c91352e59b422ffdf7bc6e542
SofKapa <85667001+SofKapa@users.noreply.github.com>
2021-07-13 13:59:14 +1000

    Animate on scroll adjust

--- _site/scripts.js
+++ _site/scripts.js
@@ -10,6 +10,8 @@ $( document ).ready(function() {
 
 
     // Animate on scroll: activate
-    AOS.init();
+    AOS.init({
+        duration: 500 // values from 0 to 3000, with step 50ms    
+    });
     
 });
